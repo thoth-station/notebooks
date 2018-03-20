fcc2210a10e53dc63e8989ea7910216f6fd828a8
Fridolin Pokorny <fridolin@redhat.com>
2018-03-20 09:50:23 +0100

    Provide environment variables to Jupyter notebook

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+# If running locally, these variables need to be provided in order to make notebooks work.
+#
+#    !!! DO NOT COMMIT CHANGES !!!
+export THOTH_DEPLOYMENT_NAME=
+export THOTH_CEPH_BUCKET=
+export THOTH_CEPH_KEY_ID=
+export THOTH_CEPH_SECRET_KEY=
+export THOTH_CEPH_HOST=
+#    !!! DO NOT COMMIT CHANGES !!!
+
 pipenv run jupyter notebook --notebook-dir=notebooks
