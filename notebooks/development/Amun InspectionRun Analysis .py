c6bb044c1fd99127a7e27c6d75e2f2858e1c7af6
Francesco Murdaca <fmurdaca@redhat.com>
2019-05-14 13:08:05 +0200

    Describe inspection job

--- notebooks/development/Amun InspectionRun Analysis .py
+++ notebooks/development/Amun InspectionRun Analysis .py
@@ -8,9 +8,9 @@
 #       format_version: '1.2'
 #       jupytext_version: 1.1.1
 #   kernelspec:
-#     display_name: thoth-notebooks
+#     display_name: Python 3
 #     language: python
-#     name: thoth-notebooks
+#     name: python3
 # ---
 
 # %% [markdown] {"toc": true}
@@ -50,12 +50,6 @@ doc_id, doc = next(inspection_store.iterate_results())  # sample
 doc['build_log'] = None
 doc
 
-# %%
-from thoth.storages import InspectionResultsStore
-
-inspection_store = InspectionResultsStore()
-inspection_store.connect()
-
 # %% [markdown]
 # ---
 
@@ -63,6 +57,68 @@ inspection_store.connect()
 # ## Describe the structure of an inspection job result
 
 # %%
+import pandas as pd
+pd.set_option('max_colwidth', 800)
+
+
+# %%
+def extract_structure_json(input_json, upper_key: str, level: int, json_structure):
+    """Convert a json file structure into a nested list showing keys depths"""
+    level += 1
+    for key in input_json.keys():
+        if type(input_json[key]) is dict:
+            json_structure.append([level, upper_key, key, [k for k in input_json[key].keys()]])
+            
+            extract_structure_json(input_json[key], f"{upper_key}__{key}", level, json_structure)
+        else:
+            json_structure.append([level, upper_key, key, input_json[key]])
+    return json_structure
+
+def filter_dataframe(json_pandas, filter_df):
+    """Filter the dataframe for a certain key, combination of keys or for a tree depth"""
+    if type(filter_df) is str:
+        available_keys = set(df["Current_key"].values)
+        available_combined_keys = set(df["Upper_keys"].values)
+        if filter_df in available_keys or filter_df in available_combined_keys:
+            ndf = df[df["Upper_keys"].str.contains(filter_df)]
+        else:
+            print("The key is not in the json")
+            ndf = "". join([f"The available keys are (WARNING: Some of the keys have no leafs): {available_keys} ", f"The available combined keys are: {available_combined_keys}"])
+            
+    elif type(filter_df) is int:
+        max_depth = df["Tree_depth"].max()
+        if filter_df <= max_depth:
+            ndf = df[df["Tree_depth"] == filter_df]
+        else:
+            ndf = f"The maximum tree depth available is: {max_depth}"
+    return ndf
+
+
+# %%
+#Create the dataframe
+df = pd.DataFrame(extract_structure_json(doc,"", 0, []))
+df.columns = ["Tree_depth", "Upper_keys", "Current_key", "Value"]
+
+# %% [markdown]
+# We can take a look at the inspection job structure from the point of view of the tree depth, considering a key or a combination of keys.
+
+# %%
+filter_dataframe(df, 1)
+
+# %%
+filter_dataframe(df, 2)
+
+# %%
+filter_dataframe(df, 3)
+
+# %%
+filter_dataframe(df, 4)
+
+# %%
+filter_dataframe(df, 5)
+
+# %%
+filter_dataframe(df, 6)
 
 # %% [markdown]
 # ---
